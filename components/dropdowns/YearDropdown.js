c3f5a1a3a7832329de38da749508cb71668fc5a0
seazyns <yashpatolia1@gmail.com>
2024-09-05 14:00:29 -0400

    update year dropdown

--- components/dropdowns/YearDropdown.js
+++ components/dropdowns/YearDropdown.js
@@ -6,7 +6,7 @@ import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button } from "@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretDown, faCheck } from "@fortawesome/free-solid-svg-icons";
 
-export default function YearDropdown(props) {
+export default function YearDropdown() {
   const [selectedKeys, setSelectedKeys] = React.useState(new Set([]));
   const selectedValue = React.useMemo(() => Array.from(selectedKeys).join(", "), [selectedKeys]);
 
@@ -14,7 +14,7 @@ export default function YearDropdown(props) {
     <Dropdown>
       <DropdownTrigger>
         <Button variant="bordered" className={`${styles.btn} capitalize`} disableRipple>
-          {selectedValue && selectedKeys.size > 3 ? <p>{selectedKeys.size} Selected</p> : <p>{selectedValue || props.label}</p>}
+          {selectedValue && selectedKeys.size > 3 ? <p>{selectedKeys.size} Selected</p> : <p>{selectedValue || "Year"}</p>}
           <FontAwesomeIcon icon={faCaretDown} style={{ color: "#ffffff" }} />
         </Button>
       </DropdownTrigger>
@@ -28,6 +28,9 @@ export default function YearDropdown(props) {
         onSelectionChange={setSelectedKeys}
         className={styles.menu}
       >
+        <DropdownItem key="2009" className={styles.item} selectedIcon={selectedKeys.has("2009") ? <FontAwesomeIcon icon={faCheck} /> : null}>
+          2009
+        </DropdownItem>
         <DropdownItem key="2010" className={styles.item} selectedIcon={selectedKeys.has("2010") ? <FontAwesomeIcon icon={faCheck} /> : null}>
           2010
         </DropdownItem>
@@ -42,6 +45,24 @@ export default function YearDropdown(props) {
         </DropdownItem>
         <DropdownItem key="2014" className={styles.item} selectedIcon={selectedKeys.has("2014") ? <FontAwesomeIcon icon={faCheck} /> : null}>
           2014
+        </DropdownItem>
+        <DropdownItem key="2015" className={styles.item} selectedIcon={selectedKeys.has("2015") ? <FontAwesomeIcon icon={faCheck} /> : null}>
+          2015
+        </DropdownItem>
+        <DropdownItem key="2016" className={styles.item} selectedIcon={selectedKeys.has("2016") ? <FontAwesomeIcon icon={faCheck} /> : null}>
+          2016
+        </DropdownItem>
+        <DropdownItem key="2017" className={styles.item} selectedIcon={selectedKeys.has("2017") ? <FontAwesomeIcon icon={faCheck} /> : null}>
+          2017
+        </DropdownItem>
+        <DropdownItem key="2018" className={styles.item} selectedIcon={selectedKeys.has("2018") ? <FontAwesomeIcon icon={faCheck} /> : null}>
+          2018
+        </DropdownItem>
+        <DropdownItem key="2019" className={styles.item} selectedIcon={selectedKeys.has("2019") ? <FontAwesomeIcon icon={faCheck} /> : null}>
+          2019
+        </DropdownItem>
+        <DropdownItem key="2020" className={styles.item} selectedIcon={selectedKeys.has("2020") ? <FontAwesomeIcon icon={faCheck} /> : null}>
+          2020
         </DropdownItem>
       </DropdownMenu>
     </Dropdown>
